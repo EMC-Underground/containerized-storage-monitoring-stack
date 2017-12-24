dce27d8ccd8a006acb018c0d3d823d994578c681
Dana Young <dyoung@jumpbox.jumpbox.pac.lab>
2017-12-24 08:21:20 +0000

    automated array config discovery

--- collect-ecs.js
+++ collect-ecs.js
@@ -1,25 +1,128 @@
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0" // Avoids DEPTH_ZERO_SELF_SIGNED_CERT error for self-signed certificates
 
-var request = require('request');
+var request = require('request'),
+	YAML = require('yamljs'),
+	xml2js = require('xml2js'),
+	util = require('util');
 
-var ip = '10.4.44.4'
-var token = 'BAAcN0ZVSWxmRDJwTlNPWUpOTGh0c1lYR1kzWUdvPQMAjAQASHVybjpzdG9yYWdlb3M6VmlydHVhbERhdGFDZW50ZXJEYXRhOmM2ZjZmY2JjLWUwNDUtNDJlZC1hZDlmLWY2NTBjNTViMzM3NgIADTE1MTM5MTIwMjMwNTMDAC51cm46VG9rZW46ZGRkMTJkZDctYTI3NC00ZjI1LTkwYzktNTlmNWNhZjZjOTM0AgAC0A8='
-var storagepool = 'urn:storageos:VirtualArray:36453c8c-6927-4729-916d-dbdcf93885b4'
-var replgroup = 'Replication_Group:urn:storageos:ReplicationGroupInfo:8d618c50-9bb5-4d2c-98f3-54537dd81666:global'
+var parser = new xml2js.Parser();
+var ECSconfig = {};
 
-var options = {
-	//uri: 'https://' + ':4443/dashboard/storagepools/' + storagepool
-	uri: 'https://' + ip + ':4443/dashboard/zones/localzone/',
-	headers: {'X-SDS-AUTH-TOKEN': token}
+if (ECSconfig.token == null) {
+	getECSConfig(function(ECSconfigData) {
+		ECSconfig = ECSconfigData;
+		getECSStats(function(stats) {
+			try {
+				console.log('diskSpaceTotalCurrent = ' + stats.diskSpaceTotalCurrent)
+				console.log('numNodes = ' + stats.numNodes)
+				console.log('numGoodNodes = ' + stats.numGoodNodes)
+				console.log('numBadNodes = ' + stats.numBadNodes)
+			}
+			catch(e) {
+				console.log('missing stat:')
+				console.log(e)
+			}	
+		})	
+	})
+} else {
+	console.log('ECS config already loaded')
+	getECSStats(function(stats) {
+		try {
+			console.log('diskSpaceTotalCurrent = ' + stats.diskSpaceTotalCurrent)
+		}
+		catch(e) {
+			console.log('missing stat:')
+			console.log(e)
+		}	
+	})	
 }
 
-request.get(options, callback);
+function getECSStats (callback) {
+	var ECSstats = {}
 
-function callback(error, response, body) {
-	console.log('error = ')
-	console.log(error)
-	console.log('body = ')
-	console.log(body)
-	console.log('parsed body = ')
-	console.log(JSON.parse(body))
+	ECSDataCalls  = [
+		'https://' + ECSconfig.ip + ':4443/dashboard/zones/localzone/', 
+		// 'https://' + ECSconfig.ip + ':4443/dashboard/storagepools/' + ECSconfig.storagepool, // stats to potentially add later
+		// 'https://' + ECSconfig.ip + ':4443/dashboard/replicationgroups/' + ECSconfig.replgroup, // currently no replication in place
+	]
+
+	ECSDataCalls.forEach(function(dataCall) {
+		dataOptions = {
+			uri: dataCall,
+			headers: {'X-SDS-AUTH-TOKEN': ECSconfig.token}
+		}
+
+		request.get(dataOptions, function(error, response, body) {
+			if (error) {
+				console.log(error)		
+			} else {
+				var stats = JSON.parse(body)
+				// console.log('stats = ')
+				// console.log(stats)
+				stats['numNodes'] != null ? ECSstats.numNodes = stats['numNodes'] : void 0
+				stats['numGoodNodes'] != null ? ECSstats.numGoodNodes = stats['numGoodNodes'] : void 0
+				stats['numBadNodes'] != null ? ECSstats.numBadNodes = stats['numBadNodes'] : void 0
+				stats['diskSpaceTotalCurrent'][0]['Space'] != null ? ECSstats.diskSpaceTotalCurrent = stats['diskSpaceTotalCurrent'][0]['Space'] : void 0
+				callback(ECSstats)		
+			}
+		});			
+	})
 }
+
+function getECSConfig (callback) {
+	var configData = YAML.load('emcecs-config.yml')
+	var uri = 'https://' + configData.ip + ':4443/login'
+	var creds = configData.login + ':' + configData.pwd
+	var encodedCreds = new Buffer(creds).toString('base64') // note for node versions newer than 4.5, use Buffer.from - https://stackoverflow.com/questions/6182315/how-to-do-base64-encoding-in-node-js
+
+	var options = {
+		uri: 'https://' + configData.ip + ':4443/login',
+		headers: {Authorization: 'Basic ' + encodedCreds}
+	}
+
+	request.get(options, function(error, response, body) {
+		if (error) {
+			console.log(error)		
+		} else {
+			configData.token = response.headers['x-sds-auth-token']
+		
+			ECSConfigCalls = [
+				// get storagepool ID
+				'https://' + configData.ip + ':4443/vdc/data-services/varrays', 
+				// get replication group ID
+				'https://' + configData.ip + ':4443//vdc/data-service/vpools'
+			]
+
+			ECSConfigCalls.forEach(function(configCall) {
+				configOptions = {
+					uri: configCall,
+					headers: {'X-SDS-AUTH-TOKEN': configData.token}
+				}
+
+				request.get(configOptions, function(error, response, body) {
+					if (error) {
+						console.log(error)		
+					} else {
+						parser.parseString(body, function (err, result) {
+							// log output to full depth of resulting object
+							// console.log(util.inspect(result, false, null))
+							result.varrays != null ? configData.storagepool = result.varrays.varray[0].id[0] : void 0
+							result.data_service_vpools != null ? configData.replgroup = result.data_service_vpools.data_service_vpool[0].id[0] : void 0
+							if (configData.storagepool != null && configData.replgroup != null) {
+								// console.log('storagepool = ' + configData.storagepool)
+								// console.log('replgroup = ' + configData.replgroup)
+								callback(configData)
+							}
+						});
+					}			
+				});			
+			})
+		}	
+	});	
+}
+
+
+
+
+
+
